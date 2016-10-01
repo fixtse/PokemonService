94cccda9313c5d3572528fa58d87abc7759d4b8c
Hernan Quintana <hernan@jsatch.com>
2016-10-01 05:53:41 -0500

    corrigiendo

--- src/main/java/pe/edu/ulima/ulpokemonapi/ulpokemonapi/dto/JsonTransformer.java
+++ src/main/java/pe/edu/ulima/ulpokemonapi/ulpokemonapi/dto/JsonTransformer.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pe.edu.ulima.ulpokemonapi.ulpokemonapi.dto;
 
 import com.google.gson.Gson;
 import spark.ResponseTransformer;
 
-/**
- *
- * @author hernan
- */
 public class JsonTransformer implements ResponseTransformer{
     private Gson gson = new Gson();
 
