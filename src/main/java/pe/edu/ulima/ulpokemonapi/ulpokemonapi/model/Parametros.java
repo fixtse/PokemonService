a71f54ce3f26fa65b7ed7e39ea32dc920bd6667b
Hernan Quintana <hernan@jsatch.com>
2016-10-01 05:58:21 -0500

    fix

--- src/main/java/pe/edu/ulima/ulpokemonapi/ulpokemonapi/model/Parametros.java
+++ src/main/java/pe/edu/ulima/ulpokemonapi/ulpokemonapi/model/Parametros.java
@@ -3,7 +3,7 @@ package pe.edu.ulima.ulpokemonapi.ulpokemonapi.model;
 
 public class Parametros {
     //public static final String URL ="jdbc:postgresql://localhost:5432/ul-pokemon";
-    public static final String URL="jdbc:postgres://ec2-54-243-201-19.compute-1.amazonaws.com:5432/d5f5enir9g5p3n";
+    public static final String URL="jdbc:postgresql://ec2-54-243-201-19.compute-1.amazonaws.com:5432/d5f5enir9g5p3n";
     public static final String USER = "vsyxnzcjejimvf";
     public static final String PASSWORD = "6S8nMe_jssr_Yim1QVWAEDLSK0";
 }
