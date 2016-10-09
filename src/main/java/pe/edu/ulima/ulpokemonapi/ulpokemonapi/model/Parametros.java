3980294a13d8c56fb38f0d9074892411b65952ae
chfernandezrios <chfernandezrios>
2016-10-08 20:39:25 -0500

    Corrección de schema de URL de DB

--- src/main/java/pe/edu/ulima/ulpokemonapi/ulpokemonapi/model/Parametros.java
+++ src/main/java/pe/edu/ulima/ulpokemonapi/ulpokemonapi/model/Parametros.java
@@ -3,7 +3,7 @@ package pe.edu.ulima.ulpokemonapi.ulpokemonapi.model;
 public class Parametros {
 
     public final static String DRIVER_CLASS = "com.mysql.jdbc.Driver";
-    public static final String URL ="jdbc:mysql://sql3.freemysqlhosting.net:3306/sql3137744";
+    public static final String URL ="jdbc:mysql://sql3.freemysqlhosting.net:3306/sql3139318";
     public static final String USER = "sql3139318";
     public static final String PASSWORD = "wrzdW3qHr3";
 }
