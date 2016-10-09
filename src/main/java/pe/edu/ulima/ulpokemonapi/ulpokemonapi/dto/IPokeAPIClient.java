5f0788c0a66e98abaa3f6a585e8db4d595fc9d10
chfernandezrios <chfernandezrios>
2016-10-09 13:17:58 -0500

    Nada

--- src/main/java/pe/edu/ulima/ulpokemonapi/ulpokemonapi/dto/IPokeAPIClient.java
+++ src/main/java/pe/edu/ulima/ulpokemonapi/ulpokemonapi/dto/IPokeAPIClient.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pe.edu.ulima.ulpokemonapi.ulpokemonapi.dto;
 
 import pe.edu.ulima.ulpokemonapi.ulpokemonapi.dto.pokemon.PokeAPIResponse;
